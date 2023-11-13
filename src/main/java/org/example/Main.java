2eec2a62df3a122424f38ff0cd741cbffe8ffa49
adi356 <adi356@umsystem.edu>
2023-11-13 16:03:36 -0700

    whole project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,73 @@
+package org.example;
+
+import com.opencsv.CSVWriter;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        String apiUrl = "https://api.github.com/repos/twbs/bootstrap/releases";
+        List<String[]> data = new ArrayList<>();
+
+        try {
+            URL url = new URL(apiUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Accept", "application/json");
+            connection.connect();
+
+            InputStream responseStream = connection.getInputStream();
+            JSONTokener tokener = new JSONTokener(responseStream);
+
+            JSONArray releases = new JSONArray(tokener);
+
+            for (int i = 0; i < releases.length(); i++) {
+                JSONObject release = releases.getJSONObject(i);
+                String createdDate = release.getString("created_at");
+                String tagName = release.getString("tag_name");
+
+                // Fetching the URL for the distribution zip file
+                JSONArray assets = release.getJSONArray("assets");
+                String zipUrl = null;
+                for (int j = 0; j < assets.length(); j++) {
+                    JSONObject asset = assets.getJSONObject(j);
+                    if (asset.getString("name").endsWith(".zip")) {
+                        zipUrl = asset.getString("browser_download_url");
+                        break;
+                    }
+                }
+
+                data.add(new String[]{createdDate, tagName, zipUrl});
+            }
+
+            writeToCSV(data);
+            System.out.println("Data written to CSV file successfully.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeToCSV(List<String[]> data) throws IOException {
+        String csvFile = "bootstrap_releases.csv";
+        FileWriter outputfile = new FileWriter(csvFile);
+        CSVWriter writer = new CSVWriter(outputfile);
+
+        String[] header = {"Created Date", "Tag name", "URL for the distribution zip file"};
+        writer.writeNext(header);
+
+        writer.writeAll(data);
+
+        writer.close();
+    }
+}
