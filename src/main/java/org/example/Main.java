6c563f465258316ef785bf5546f4b170f2a3e65b
adi356 <adi356@umsystem.edu>
2023-11-13 16:11:57 -0700

    whole project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,8 +12,8 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
+
 public class Main {
     public static void main(String[] args) {
         String apiUrl = "https://api.github.com/repos/twbs/bootstrap/releases";
